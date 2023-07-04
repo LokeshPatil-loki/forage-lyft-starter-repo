d2262ef3702f6d791916cf2a44fcb38e4a8da8e7
LokeshPatil-loki <lokesh1404@outlook.com>
2023-07-05 01:48:42 +0530

    Refractored WilloughbyEngine class

--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -2,10 +2,10 @@ from typing_extensions import override
 from .engine import Engine
 
 class WilloughbyEngine(Engine):
-    def __init__(self, current_mileage, last_service_mileage):
-        self.__current_mileage = current_mileage
-        self.__last_service_mileage = last_service_mileage
+    def __init__(self, current_mileage:int, last_service_mileage:int):
+        self.__current_mileage:int = current_mileage
+        self.__last_service_mileage:int = last_service_mileage
 
     @override
-    def needs_service(self):
+    def needs_service(self) -> bool:
         return self.__current_mileage - self.__last_service_mileage > 60000
