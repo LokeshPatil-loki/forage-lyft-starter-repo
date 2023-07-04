b1218a3e27e740dd32aaae8ca1c81dbd6e373147
LokeshPatil-loki <lokesh1404@outlook.com>
2023-07-05 01:34:52 +0530

    Created SpindlerBattery. implemented needs_service()

---
+++ battery/spindler_battery.py
@@ -0,0 +1,14 @@
+from datetime import date
+from typing_extensions import override
+from .battery import Battery
+
+class SpindlerBattery(Battery):
+    def __init__(self,last_service_date: date,current_date:date):
+        self.__last_service_date = last_service_date
+        self.__current_date = date.today()
+    
+    @override
+    def needs_service(self) -> bool:
+        self.__current_date = date.today()
+        years = (self.__current_date - self.__last_service_date).days * 365
+        return years >= 2
