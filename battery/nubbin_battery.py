eabba11625b281449cd65d6f72265c72f199dfe8
LokeshPatil-loki <lokesh1404@outlook.com>
2023-07-05 09:36:32 +0530

    fixed needs_service() to pass test cases

--- battery/nubbin_battery.py
+++ battery/nubbin_battery.py
@@ -12,4 +12,4 @@ class NubbinBattery(Battery):
     @override
     def needs_service(self) -> bool:
         date_which_battery_should_be_serviced_by = add_years_to_date(self.__last_service_date,4)
-        return date_which_battery_should_be_serviced_by < self.__current_date
+        return date_which_battery_should_be_serviced_by <= self.__current_date
--- battery/spindler_battery.py
+++ battery/spindler_battery.py
@@ -12,4 +12,4 @@ class SpindlerBattery(Battery):
     @override
     def needs_service(self) -> bool:
         date_which_battery_should_be_serviced_by = add_years_to_date(self.__last_service_date,2)
-        return date_which_battery_should_be_serviced_by < self.__current_date
+        return date_which_battery_should_be_serviced_by <= self.__current_date
